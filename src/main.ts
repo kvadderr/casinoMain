e9e892153ca91b0338d01d5e702378851a43f8dd
Сергей Похабов <itigertechno@gmail.com>
2024-03-24 16:07:17 +0500

    fis

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
 
   const config = new DocumentBuilder()
