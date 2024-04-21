21a48297180af06e001a96ca87dfbd6398dadb2e
Сергей Похабов <itigertechno@gmail.com>
2024-04-22 04:50:48 +0500

    fix

--- src/game-history/game-history.service.ts
+++ src/game-history/game-history.service.ts
@@ -14,6 +14,7 @@ export class GameHistoryService {
   ) { }
 
   create(createGameHistoryDto: CreateGameHistoryDto) {
+    console.log(createGameHistoryDto)
     return this.freespinRepository.save(createGameHistoryDto)
   }
 
